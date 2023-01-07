600b4bf915d3642ba49188c68aebedde467dd352
Nirmit <nirmitsabharwal15@gmail.com>
2023-01-07 23:16:42 +0530

    mumbai network added

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,24 @@
 require('babel-register');
 require('babel-polyfill');
 
+const mumbaiRPC = '';
+const MNEMONIC = '';
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" 
+      network_id: "*"
+    },
+    mumbai: {
+      provider: () => new HDWalletProvider(MNEMONIC, mumbaiRPC),
+      network_id: 80001,
+      confirmations: 1,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      gas: 6000000,
+      gasPrice: 10000000000,
     },
   },
   contracts_directory: './contracts/',
